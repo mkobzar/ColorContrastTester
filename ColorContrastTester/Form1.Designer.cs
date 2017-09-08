ab9bfba24d64d370d94c7fe5c4f0cc671414f8c3
Mike K <Mike K>
2017-09-08 00:32:36 -0500

    tester done

---
+++ ColorContrastTester/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ColorContrastTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtColor1 = new System.Windows.Forms.TextBox();
+            this.color2btn = new System.Windows.Forms.Button();
+            this.txtColor2 = new System.Windows.Forms.TextBox();
+            this.btnPickColor = new System.Windows.Forms.Button();
+            this.txtRatio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.color1btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtColor1
+            // 
+            this.txtColor1.Location = new System.Drawing.Point(339, 154);
+            this.txtColor1.Name = "txtColor1";
+            this.txtColor1.Size = new System.Drawing.Size(60, 20);
+            this.txtColor1.TabIndex = 2;
+            // 
+            // color2btn
+            // 
+            this.color2btn.BackColor = System.Drawing.Color.DimGray;
+            this.color2btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.color2btn.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.color2btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.color2btn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.color2btn.Location = new System.Drawing.Point(446, 38);
+            this.color2btn.Name = "color2btn";
+            this.color2btn.Size = new System.Drawing.Size(100, 100);
+            this.color2btn.TabIndex = 1;
+            this.color2btn.Text = "Color 2";
+            this.color2btn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.color2btn.UseVisualStyleBackColor = false;
+            this.color2btn.MouseDown += new System.Windows.Forms.MouseEventHandler(this.color2btn_MouseDown);
+            this.color2btn.MouseUp += new System.Windows.Forms.MouseEventHandler(this.color2btn_MouseUp);
+            // 
+            // txtColor2
+            // 
+            this.txtColor2.Location = new System.Drawing.Point(470, 154);
+            this.txtColor2.Name = "txtColor2";
+            this.txtColor2.Size = new System.Drawing.Size(60, 20);
+            this.txtColor2.TabIndex = 3;
+            // 
+            // btnPickColor
+            // 
+            this.btnPickColor.BackColor = System.Drawing.Color.Transparent;
+            this.btnPickColor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnPickColor.Enabled = false;
+            this.btnPickColor.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.btnPickColor.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnPickColor.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnPickColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPickColor.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnPickColor.Location = new System.Drawing.Point(20, 30);
+            this.btnPickColor.Margin = new System.Windows.Forms.Padding(5);
+            this.btnPickColor.Name = "btnPickColor";
+            this.btnPickColor.Size = new System.Drawing.Size(200, 200);
+            this.btnPickColor.TabIndex = 7;
+            this.btnPickColor.TabStop = false;
+            this.btnPickColor.UseVisualStyleBackColor = false;
+            // 
+            // txtRatio
+            // 
+            this.txtRatio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRatio.Location = new System.Drawing.Point(470, 196);
+            this.txtRatio.Name = "txtRatio";
+            this.txtRatio.Size = new System.Drawing.Size(57, 23);
+            this.txtRatio.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(318, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Contrast Ratio";
+            // 
+            // color1btn
+            // 
+            this.color1btn.BackColor = System.Drawing.Color.Black;
+            this.color1btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.color1btn.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.color1btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.color1btn.ForeColor = System.Drawing.SystemColors.Control;
+            this.color1btn.Location = new System.Drawing.Point(316, 38);
+            this.color1btn.Name = "color1btn";
+            this.color1btn.Size = new System.Drawing.Size(100, 100);
+            this.color1btn.TabIndex = 0;
+            this.color1btn.Text = "Color 1";
+            this.color1btn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.color1btn.UseVisualStyleBackColor = false;
+            this.color1btn.MouseDown += new System.Windows.Forms.MouseEventHandler(this.color1btn_MouseDown);
+            this.color1btn.MouseUp += new System.Windows.Forms.MouseEventHandler(this.color1btn_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(530, 20);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(540, 2);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(558, 247);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRatio);
+            this.Controls.Add(this.txtColor2);
+            this.Controls.Add(this.txtColor1);
+            this.Controls.Add(this.color2btn);
+            this.Controls.Add(this.color1btn);
+            this.Controls.Add(this.btnPickColor);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ColorContrastTester";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtColor1;
+        private System.Windows.Forms.Button color2btn;
+        private System.Windows.Forms.Button color1btn;
+        private System.Windows.Forms.TextBox txtColor2;
+        private System.Windows.Forms.Button btnPickColor;
+        private System.Windows.Forms.TextBox txtRatio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ColorContrastTester/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ColorContrastTester
+{
+    public partial class Form1 : Form
+    {
+        private readonly Timer _getColorTimer;
+        private Bitmap _sampleBitmap;
+        private bool _hasSampled;
+        private const int SampleSize = 5;
+        private readonly Color[,] _previewColors;
+        private Color _sampleColor = Color.Black;
+
+        private Color SampleColor
+        {
+            get => _sampleColor;
+            set
+            {
+                _sampleColor = value;
+                if (_isColor1)
+                {
+                    color1btn.BackColor = _sampleColor;
+                    color2btn.ForeColor = _sampleColor;
+                }
+                else
+                {
+                    color1btn.ForeColor = _sampleColor;
+                    color2btn.BackColor = _sampleColor;
+                }
+                BackColor = _sampleColor;
+            }
+        }
+
+        private bool _isColor1 = true;
+        private const int PreviewSize = 200;
+        private const int PreviewX = 20;
+        private const int PreviewY = 30;
+        public const int WmNclbuttondown = 0xA1;
+        public const int HtCaption = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            TopMost = true;
+            _getColorTimer = new Timer {Interval = 10};
+            _getColorTimer.Tick += GetColorTimerTick;
+            _previewColors = new Color[PreviewSize, PreviewSize];
+        }
+
+
+        /// <summary>
+        /// Populates textboxes with correct color values.
+        /// </summary>
+        private void TranslateColor()
+        {
+            if (!_hasSampled)
+                return;
+
+            try
+            {
+                var htmlColor = ColorTranslator.ToHtml(SampleColor).TrimStart('#');
+                if (_isColor1)
+                    txtColor1.Text = htmlColor;
+                else
+                    txtColor2.Text = htmlColor;
+            }
+            catch
+            {
+                //ignore
+            }
+        }
+
+
+        void GetColorTimerTick(object sender, EventArgs e)
+        {
+            try
+            {
+                Screen.FromRectangle(new Rectangle(MousePosition.X, MousePosition.Y, 1, 1));
+                _sampleBitmap = GetSampleRegion(MousePosition.X, MousePosition.Y);
+                var newColor = _sampleBitmap.GetPixel(SampleSize / 2, SampleSize / 2);
+                SampleColor = newColor;
+                TranslateColor();
+                if (BackColor == newColor) Invalidate(new Rectangle(PreviewX, PreviewY, PreviewSize, PreviewSize));
+            }
+            catch
+            {
+                // ignore
+            }
+        }
+
+
+        /// <summary>
+        /// Returns the sample region.
+        /// </summary>
+        /// <param name="mouseX">Mouse x position.</param>
+        /// <param name="mouseY">Mouse y position.</param>
+        /// <returns></returns>
+        private Bitmap GetSampleRegion(int mouseX, int mouseY)
+        {
+            var bmp = new Bitmap(SampleSize, SampleSize, PixelFormat.Format32bppArgb);
+            var gfxScreenshot = Graphics.FromImage(bmp);
+            gfxScreenshot.CopyFromScreen(mouseX - SampleSize / 2, mouseY - SampleSize / 2, 0, 0,
+                new Size(SampleSize, SampleSize));
+            gfxScreenshot.Save();
+            gfxScreenshot.Dispose();
+            return bmp;
+        }
+
+        private static Color GetContrastColor(Color color)
+        {
+            var yiq = (color.R * 299 + color.G * 587 + color.B) / 1000;
+            return yiq >= 131.5 ? Color.FromArgb(255, 33, 33, 33) : Color.White;
+        }
+
+        protected override void
+            OnPaint(PaintEventArgs paintEvnt)
+        {
+            var gfx = paintEvnt.Graphics;
+            var pen = new Pen(Color.FromArgb(255, 255, 0, 0), 2);
+            const int blockSize = PreviewSize / SampleSize;
+
+            color1btn.FlatAppearance.BorderColor = color2btn.FlatAppearance.BorderColor = GetContrastColor(SampleColor);
+
+            for (var i = 0; i < SampleSize; i++)
+            {
+                for (var j = 0; j < SampleSize; j++)
+                {
+                    if (_sampleBitmap != null)
+                    {
+                        _previewColors[i, j] = _sampleBitmap.GetPixel(i, j);
+                    }
+                    Brush brush = new SolidBrush(_previewColors[i, j]);
+                    gfx.FillRectangle(brush,
+                        new Rectangle(PreviewX + i * blockSize, PreviewY + j * blockSize, blockSize, blockSize));
+
+                }
+            }
+
+            if (_hasSampled)
+            {
+                gfx.DrawRectangle(pen, PreviewX + blockSize * (SampleSize / 2), PreviewY + blockSize * (SampleSize / 2),
+                    blockSize, blockSize);
+            }
+
+            if (_sampleBitmap == null) return;
+            _sampleBitmap.Dispose();
+            _sampleBitmap = null;
+        }
+
+
+        private void color1btn_MouseDown(object sender, MouseEventArgs e)
+        {
+            _isColor1 = true;
+            _getColorTimer.Start();
+            if (!_hasSampled)
+            {
+                color1btn.BackgroundImage = null;
+                _hasSampled = true;
+            }
+            Cursor = Cursors.Cross;
+        }
+
+
+        private void color2btn_MouseDown(object sender, MouseEventArgs e)
+        {
+            _isColor1 = false;
+            _getColorTimer.Start();
+            if (!_hasSampled)
+            {
+                color2btn.BackgroundImage = null;
+                _hasSampled = true;
+            }
+            Cursor = Cursors.Cross;
+        }
+
+
+        private void color1btn_MouseUp(object sender, MouseEventArgs e)
+        {
+            color1btn.BackColor = _sampleColor;
+            colorbtn_MouseUp();
+        }
+
+        private void colorbtn_MouseUp()
+        {
+            _getColorTimer.Stop();
+            Cursor = Cursors.Default;
+
+            if (_sampleBitmap != null)
+            {
+                _sampleBitmap.Dispose();
+                _sampleBitmap = null;
+            }
+            GC.Collect();
+            BackColor = Color.Black;
+            if (!string.IsNullOrEmpty(txtColor1.Text) && !string.IsNullOrEmpty(txtColor2.Text))
+                txtRatio.Text = GetContrastRatio(txtColor1.Text, txtColor2.Text);
+        }
+
+        private void color2btn_MouseUp(object sender, MouseEventArgs e)
+        {
+            color2btn.BackColor = _sampleColor;
+            colorbtn_MouseUp();
+        }
+
+        private string GetContrastRatio(string color1, string color2)
+        {
+            try
+            {
+                var luminance1 = GetLums(color1);
+                var luminance2 = GetLums(color2);
+                return (Math.Round(
+                            (Math.Max(luminance1, luminance2) + 0.05) / (Math.Min(luminance1, luminance2) + 0.05) * 10,
+                            1) / 10).ToString(CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        private static double GetLums(string colorHex)
+        {
+            colorHex = colorHex.TrimStart('#');
+            if (string.IsNullOrEmpty(colorHex) || colorHex.Length != 6)
+                return Double.NaN;
+            var r8Bit = Convert.ToInt32(colorHex.Substring(0, 2), 16);
+            var g8Bit = Convert.ToInt32(colorHex.Substring(2, 2), 16);
+            var b8Bit = Convert.ToInt32(colorHex.Substring(4, 2), 16);
+
+            var rsRgb = r8Bit / 255.0;
+            var gsRgb = g8Bit / 255.0;
+            var bsRgb = b8Bit / 255.0;
+            // Calculate luminance
+            var r = rsRgb <= 0.03928 ? rsRgb / 12.92 : Math.Pow((rsRgb + 0.055) / 1.055, 2.4);
+            var g = gsRgb <= 0.03928 ? gsRgb / 12.92 : Math.Pow((gsRgb + 0.055) / 1.055, 2.4);
+            var b = bsRgb <= 0.03928 ? bsRgb / 12.92 : Math.Pow((bsRgb + 0.055) / 1.055, 2.4);
+            var ret = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+            return ret;
+        }
+
+
+        private void Form_MouseDown(MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            ReleaseCapture();
+            SendMessage(Handle, WmNclbuttondown, HtCaption, 0);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Form_MouseDown(e);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Form_MouseDown(e);
+        }
+    }
+}
